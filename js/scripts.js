d39d3c27a333113fc2b2631a16201e93fede819f
James Provance <jb24x7@gmail.com>
2023-01-13 09:49:11 -0800

    adding and hiding results

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,57 @@
+function hideResult() {
+  document.getElementById("moderate").setAttribute("class", "hidden");
+  document.getElementById("liberal").setAttribute("class", "hidden");
+  document.getElementById("conservative").setAttribute("class", "hidden");
+}
+
+
 var myQuestions = [
   {
     question: "Sample Question",
     answers: {
-      a: 'yes',
-      b: 'no',
+      YES: '',
+      NO: '',
     },
-    correctAnswer: 'a'
+    correctAnswer: 'YES'
   },
   {
     question: "Sample Question",
     answers: {
-      a: 'yes',
-      b: 'no',
+      YES: '',
+      NO: '',
 
     },
-    correctAnswer: 'a'
+    correctAnswer: 'YES'
+  },
+
+  {
+    question: "Sample Question",
+    answers: {
+      YES: '',
+      NO: '',
+
+    },
+    correctAnswer: 'YES'
+  },
+
+  {
+    question: "Sample Question",
+    answers: {
+      YES: '',
+      NO: '',
+
+    },
+    correctAnswer: 'YES'
+  },
+
+  {
+    question: "Sample Question",
+    answers: {
+      YES: '',
+      NO: '',
+
+    },
+    correctAnswer: 'YES'
   }
 ];
 
@@ -27,20 +64,16 @@ generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
   function showQuestions(questions, quizContainer){
-    // we'll need a place to store the output and the answer choices
+
     var output = [];
     var answers;
 
-    // for each question...
     for(var i=0; i<questions.length; i++){
       
-      // first reset the list of answers
       answers = [];
 
-      // for each available answer...
       for(letter in questions[i].answers){
 
-        // ...add an html radio button
         answers.push(
           '<label>'
             + '<input type="radio" name="question'+i+'" value="'+letter+'">'
@@ -50,56 +83,46 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
         );
       }
 
-      // add this question and its answers to the output
       output.push(
         '<div class="question">' + questions[i].question + '</div>'
         + '<div class="answers">' + answers.join('') + '</div>'
       );
     }
 
-    // finally combine our output list into one string of html and put it on the page
     quizContainer.innerHTML = output.join('');
   }
 
 
   function showResults(questions, quizContainer, resultsContainer){
     
-    // gather answer containers from our quiz
     var answerContainers = quizContainer.querySelectorAll('.answers');
     
-    // keep track of user's answers
+
     var userAnswer = '';
     var numCorrect = 0;
     
-    // for each question...
     for(var i=0; i<questions.length; i++){
 
-      // find selected answer
       userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
       
-      // if answer is correct
       if(userAnswer===questions[i].correctAnswer){
-        // add to the number of correct answers
         numCorrect++;
         
-        // color the answers green
         answerContainers[i].style.color = 'lightgreen';
       }
-      // if answer is wrong or blank
+
       else{
-        // color the answers red
         answerContainers[i].style.color = 'red';
       }
     }
 
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    if(numCorrect >>3){
+
+    }
   }
 
-  // show questions right away
   showQuestions(questions, quizContainer);
   
-  // on submit, show results
   submitButton.onclick = function(){
     showResults(questions, quizContainer, resultsContainer);
   }
